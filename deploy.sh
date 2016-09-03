03e56a8cc3df13c59cb0948f1ab617c4f12abc88
Sheikh, Haroon <haroon.sheikh@sky.uk>
2016-09-03 16:11:14 +0100

    Removing unnecessary files

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Exit on any error
-set -e
-
-sudo /opt/google-cloud-sdk/bin/gcloud docker push ${CONTAINER_REGISTRY}/${PROJECT_NAME}
-sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment ${CONTAINER_NAME} -p '{"spec":{"template":{"spec":{"containers":[{"name":"'"$CONTAINER_NAME"'","image":"'"$CONTAINER_REGISTRY"'/'"$PROJECT_NAME"':'"$CIRCLE_SHA1"'"}]}}}}'
